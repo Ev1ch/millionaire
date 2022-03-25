65908d399c5b763d0b2a80e937b2e60e070580ed
Vlad Zusko <vladzvu11@gmail.com>
2022-03-26 00:49:23 +0200

    fix: extracted numbers formatting function

--- src/components/basic/stage/index.tsx
+++ src/components/basic/stage/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import { Hexagon } from 'components/basic';
 import { IIndexedQuestion } from 'domain/question';
+import formatNumber from 'helpers/format-number';
 import styles from './stage.module.scss';
 
 const Types = {
@@ -21,7 +22,7 @@ function Stage({ question, className, type }: IStageProps) {
             <Hexagon className={styles.stage__hexagon}>
                 <p className={styles.stage__prize}>
                     &#36;
-                    {question.prize.toLocaleString('en-US')}
+                    {formatNumber(question.prize)}
                 </p>
             </Hexagon>
         </div>
--- src/components/pages/final/index.tsx
+++ src/components/pages/final/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Container } from 'components/basic';
+import formatNumber from 'helpers/format-number';
 import finalImage from 'assets/images/thumb-up.svg';
 import styles from './final.module.scss';
 
@@ -17,7 +18,7 @@ function Final({ prize, onTryAgain }: IFinalProps) {
                     <h2 className={styles.final__subtitle}>Total score:</h2>
                     <h1 className={styles.final__title}>
                         &#36;
-                        {prize.toLocaleString('en-US')}
+                        {formatNumber(prize)}
                         &nbsp;earned
                     </h1>
                     <Button className={styles.final__button} onClick={onTryAgain}>

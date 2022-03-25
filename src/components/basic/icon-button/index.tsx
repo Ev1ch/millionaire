b64883337929a7fc78e241793014dc58406b98fa
Vlad Zusko <vladzvu11@gmail.com>
2022-03-25 23:46:43 +0200

    fix: fixed basic icon button cursor

--- src/components/basic/icon-button/index.tsx
+++ src/components/basic/icon-button/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import clsx from 'clsx';
 import styles from './icon-button.module.scss';
 
 interface IIconButtonProps {
-    icon: string;
+    icon?: string;
     className?: string;
     text?: string;
+    onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
-function IconButton({ className, icon, text }: IIconButtonProps) {
+function IconButton({ className, icon, text, onClick }: IIconButtonProps) {
+    const clickHandler = (event: MouseEvent<HTMLButtonElement>) => {
+        if (!onClick) {
+            return;
+        }
+
+        onClick(event);
+    };
+
     return (
-        <button type="button" className={clsx(styles.iconButton, className)}>
-            <img src={icon} alt={text} />
+        <button type="button" className={clsx(styles.iconButton, className)} onClick={clickHandler}>
+            {icon && <img src={icon} alt={text} />}
         </button>
     );
 }
